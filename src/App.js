aa423825344e16e3641a7fc110bb14870dba363b
Yassine SAHLI <ysahl091@uottawa.ca>
2025-07-20 13:26:00 -0400

    Setup GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -5,24 +5,28 @@ import Dashboard from "./scenes/dashboard";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
 import './i18n';
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   const [theme, colorMode] = useMode();
   const [isSidebar, setIsSidebar] = useState(true);
 
   return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <div className="app">
-          <Sidebar isSidebar={isSidebar}/>
-          <main className="content">
-            <Topbar setIsSidebar={setIsSidebar} />
-              <Dashboard />
-          </main>
-        </div>
-      </ThemeProvider>
-    </ColorModeContext.Provider>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <div className="app">
+            <Sidebar isSidebar={isSidebar}/>
+            <main className="content">
+              <Topbar setIsSidebar={setIsSidebar} />
+                <Dashboard />
+            </main>
+          </div>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
+    </BrowserRouter>
+    
   );
 }
 
